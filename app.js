fbe40cbecfdf73de676a094a477d6ad4baaaf327
rittidach63rmutl <rittidachtaya63@gmail.com>
2026-08-18 23:29:24 +0700

    แก้ไขระบบ Login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const products = require("./data/produtcs.json");
+
 const produtRouter = express.Router();
 
 const app = express();  
@@ -15,14 +17,9 @@ app.set("views","./src/views");
 app.set("view engine","ejs")
 
 produtRouter.route("/").get((req,res)=>{
-    res.render("products",{
-        products:[
-            {productsTitle:'น้ำยาล้างจาน',productsDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ',productsPrice: 45},
-            {productsTitle:'น้ำยาล้างจาน 2',productsDescription: 'น้ำยาล้างจานสูตร 2 ดีมาก',productsPrice: 40},
-            {productsTitle:'น้ำยาล้างจาน 3',productsDescription: 'น้ำยาล้างจานสูตร 3 ดีมั้ง',productsPrice: 35},
-            {productsTitle:'น้ำยาล้างจาน 4',productsDescription: 'น้ำยาล้างจานสูตร 4 ดีเฉยๆ',productsPrice: 30},
-        ],
-    });
+    res.render("products",
+        products,
+    );
 });
 
 app.use("/products",produtRouter)
